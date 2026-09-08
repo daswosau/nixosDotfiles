567c384ada59df165737c2fc1a8ce32dc15fe778
daswosau <daswosau@gmail.com>
2026-09-08 13:58:31 +0200

    Package fix and virt-manager enable for qemu

--- packages.nix
+++ packages.nix
@@ -91,6 +91,25 @@
 
  	];
 
+	programs = {
+		steam = {
+			enable = true;
+			extraCompatPackages = with pkgs; [
+				proton-ge-bin
+			];
+		};
+		java = {
+			enable = true;
+			package = (pkgs.jdk21.override { enableJavaFX = true; });
+		};
+		fish = {
+			enable = true;
+		};
+		virt-manager = {
+			enable = true;
+		};
+	};
+
     programs.dconf.profiles.user.databases = [{
 		settings = {
 			"org/gnome/shell" = {
@@ -110,21 +129,10 @@
 		};
 	}];
 
-	programs.steam = {
-		enable = true;
-		extraCompatPackages = with pkgs; [
-			proton-ge-bin
-		];
+	virtualisation = {
+		libvirtd.enable = true;
+		spiceUSBRedirection.enable = true;
 	};
-
-	programs.java = {
-		enable = true;
-		package = (pkgs.jdk21.override { enableJavaFX = true; });
-	};
-
-    programs.fish = {
-        enable = true;
-    };
 
 }
 
