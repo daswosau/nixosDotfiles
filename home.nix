1f269025edf077d4d5413c517465eb2330896d64
daswosau <daswosau@gmail.com>
2026-09-08 14:38:59 +0200

    Issues with home.nix

---
+++ home.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  home.username = "daswosau";
+  home.homeDirectory = "/home/daswosau";
+  home.stateVersion = "25.05"; # Use your NixOS version string
+
+  # Enable Home Manager to manage itself
+  programs.home-manager.enable = true;
+}
