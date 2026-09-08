717152f2f2701a06ec0eafe363e61330fc46542c
daswosau <daswosau@gmail.com>
2026-09-08 14:41:01 +0200

    You're joking right

--- home.nix
+++ home.nix
@@ -3,7 +3,7 @@
 {
   home.username = "daswosau";
   home.homeDirectory = "/home/daswosau";
-  home.stateVersion = "26.05"; # Use your NixOS version string
+  home.stateVersion = "25.05"; # Use your NixOS version string
 
   # Enable Home Manager to manage itself
   programs.home-manager.enable = true;
