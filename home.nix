ef7f1d5cd0e1a0c4646f25b83c931952c200e7a5
daswosau <daswosau@gmail.com>
2026-09-08 14:42:47 +0200

    Success

--- home.nix
+++ home.nix
@@ -4,6 +4,8 @@
   home.username = "daswosau";
   home.homeDirectory = "/home/daswosau";
   home.stateVersion = "25.05"; # Use your NixOS version string
+  home.enableNixpkgsReleaseCheck = false;
+
 
   # Enable Home Manager to manage itself
   programs.home-manager.enable = true;
