a9e1dc32ce250f22cbf5216e6ca79fbf454636f6
daswosau <daswosau@gmail.com>
2026-09-08 14:41:46 +0200

    Back to flake.nix

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.yourusername = import ./home.nix;
+          home-manager.users.daswosau = import ./home.nix;
         }
 
         wsf.nixosModules.default { 
