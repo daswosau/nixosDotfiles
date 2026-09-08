09b2dc0dfc5d4c6de69280377066613521443b1d
daswosau <daswosau@gmail.com>
2026-09-08 16:13:05 +0200

    Attempt to invalidate home-manager

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     # home manager
-    home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #home-manager = {
+    #  url = "github:nix-community/home-manager/release-26.05";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
     # claude desktop
     claude-desktop-linux.url = "github:k3d3/claude-desktop-linux-flake";
     # wayland scroll factor
@@ -17,16 +17,16 @@
     };
   };
 
-  outputs = { self, nixpkgs, claude-desktop-linux, wsf, home-manager }: {
+  outputs = { self, nixpkgs, claude-desktop-linux, wsf }: {
     nixosConfigurations.dwsthinkpad = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix # input modules 
 
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.daswosau = import ./home.nix;
-        }
+        #home-manager.nixosModules.home-manager {
+        #  home-manager.useGlobalPkgs = true;
+        #  home-manager.useUserPackages = true;
+        #  home-manager.users.daswosau = import ./home.nix;
+        #}
 
         wsf.nixosModules.default { 
           programs.wsf.enable = true; 
