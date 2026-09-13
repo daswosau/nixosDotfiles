fc7260b4635f5b7a35e84ec36a52797c74365605
daswosau <daswosau@gmail.com>
2026-09-13 19:00:51 +0200

    Claude FHS fix in Packages

--- packages.nix
+++ packages.nix
@@ -77,7 +77,7 @@
 		foliate
 		audacity
 		nicotine-plus
-		inputs.claude-desktop.packages.${system}.claude-desktop-with-fhs
+		claude-desktop-with-fhs
 		github-desktop
 		davinci-resolve
 		postman
