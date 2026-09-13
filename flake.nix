95028b5e9294b2cfa696213da6cc74851c879090
daswosau <daswosau@gmail.com>
2026-09-13 18:59:16 +0200

    Added flake utils

--- flake.nix
+++ flake.nix
@@ -3,15 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
     # home manager
     #home-manager = {
     #  url = "github:nix-community/home-manager/release-26.05";
     #  inputs.nixpkgs.follows = "nixpkgs";
     #};
     # claude desktop
-    claude-desktop.url = "github:k3d3/claude-desktop-linux-flake";
-    claude-desktop.inputs.nixpkgs.follows = "nixpkgs";
-    claude-desktop.inputs.flake-utils.follows = "flake-utils";
+    claude-desktop = {
+      url = "github:k3d3/claude-desktop-linux-flake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
     # wayland scroll factor
     wsf = {
       url = "github:daniel-g-carrasco/wayland-scroll-factor";
