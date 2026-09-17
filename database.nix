9515539c1b5b3f7c31d236dcb0eb63f9320339f9
daswosau <daswosau@gmail.com>
2026-09-17 13:34:30 +0200

    Database nix fixed

--- database.nix
+++ database.nix
@@ -1,27 +1,47 @@
 { config, lib, pkgs, ... }:
 
-
 {
   services.httpd = {
     enable = true;
     adminAddr = "admin@localhost";
     enablePHP = true;
+    phpPackage = pkgs.php;
 
+    modules = [ "mpm_prefork" ];
+
+    virtualHosts = {
+      "localhost" = {
+        documentRoot = "/var/www/html";
+        locations."~ \\.php$" = {
+          handler = "application/x-httpd-php";
+        };
+      };
+    };
   };
 
   services.mysql = {
     enable = true;
     package = pkgs.mariadb;
+    initialScript = pkgs.writeText "mariadb-init" ''
+      ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+    '';
     ensureUsers = [
       {
-        name = "root";
-        ensurePermissions = { "*.*" = "ALL PRIVILEGES"; };
+        name = "phpmyadmin";
+        ensurePermissions = {
+          "*.*" = "ALL PRIVILEGES";
+        };
       }
     ];
+    ensureDatabases = [ "phpmyadmin" ];
   };
 
   services.phpmyadmin = {
     enable = true;
-    router = "httpd";
+    hostName = "localhost";
+    nginx = null;  # explicitly use Apache, not nginx
   };
+
+  # Optional: allow HTTP/HTTPS traffic
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
