768200e059da03d3ed5b49cd46bfea2643639e16
daswosau <daswosau@gmail.com>
2026-09-17 13:56:26 +0200

    Database.nix update

--- database.nix
+++ database.nix
@@ -1,41 +1,67 @@
 { config, lib, pkgs, ... }:
 
+let
+  phpmyadminSrc = pkgs.fetchFromGitHub {
+    owner = "code-lts";
+    repo = "phpmyadmin-releases";
+    rev = "upstream/version/all-languages/5.2.3";
+    hash = lib.fakeHash; # Futtatás után cseréld ki a valódi hash-re
+  };
+
+  phpmyadminConfig = pkgs.writeTextDir "config.inc.php" ''
+    <?php
+    $cfg['blowfish_secret'] = 'Nx7kQ2pLmA9sWdYr4vBhJeZ6gUfTcR3i';
+    $cfg['Servers'][1]['auth_type']     = 'cookie';
+    $cfg['Servers'][1]['host']          = '127.0.0.1';
+    $cfg['Servers'][1]['AllowNoPassword'] = false;
+    $cfg['UploadDir'] = '';
+    $cfg['SaveDir']   = '';
+  '';
+
+  phpmyadmin = pkgs.symlinkJoin {
+    name  = "phpmyadmin";
+    paths = [ phpmyadminSrc phpmyadminConfig ];
+  };
+in
 {
   services.httpd = {
-    enable = true;
+    enable    = true;
     adminAddr = "admin@localhost";
     enablePHP = true;
-    phpPackage = pkgs.php;
+    phpPackage = pkgs.php.override { ztsSupport = true; };
 
-    modules = [ "mpm_prefork" ];
+    virtualHosts."localhost" = {
+      documentRoot = "/var/www/html";
+      extraConfig = ''
+        <Directory "/var/www/html">
+          Options -Indexes +FollowSymLinks
+          AllowOverride All
+          Require all granted
+        </Directory>
 
-    virtualHosts = {
-      "localhost" = {
-        documentRoot = "/var/www/html";
-        locations."~ \\.php$" = {
-          handler = "application/x-httpd-php";
-        };
-      };
+        Alias /phpmyadmin ${phpmyadmin}
+        <Directory "${phpmyadmin}">
+          DirectoryIndex index.php
+          Options -Indexes +FollowSymLinks
+          AllowOverride All
+          Require all granted
+        </Directory>
+      '';
     };
   };
 
   services.mysql = {
-    enable = true;
+    enable  = true;
     package = pkgs.mariadb;
+    ensureDatabases = [ "phpmyadmin" ];
     ensureUsers = [
       {
         name = "phpmyadmin";
         ensurePermissions = {
-          "*.*" = "ALL PRIVILEGES";
+          "phpmyadmin.*" = "ALL PRIVILEGES";
         };
       }
     ];
-    ensureDatabases = [ "phpmyadmin" ];
-  };
-
-  services.phpmyadmin = {
-    enable = true;
-    hostName = "localhost";
   };
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
